b877f0087ce9cfc8e7526c3491867cf6dfead808
Sergio Milla <sergiomse@gmail.com>
2020-12-21 16:02:54 +0100

    Adding basic theme creation

--- Styling.fs
+++ Styling.fs
@@ -6,5 +6,37 @@ open Utils
 
 
 module Styling =
+ 
+  type IFontStyle = { fontFamily: string }
+  
+  type IThemePalette =
+      { themePrimary: string
+        themeLighterAlt: string
+        themeLighter: string
+        themeLight: string
+        themeTertiary: string
+        themeSecondary: string
+        themeDarkAlt: string
+        themeDark: string
+        themeDarker: string
+        neutralLighterAlt: string
+        neutralLighter: string
+        neutralLight: string
+        neutralQuaternaryAlt: string
+        neutralQuaternary: string
+        neutralTertiaryAlt: string
+        neutralTertiary: string
+        neutralSecondary: string
+        neutralPrimaryAlt: string
+        neutralPrimary: string
+        neutralDark: string
+        black: string
+        white: string }
+
+  type ITheme = { defaultFontStyle: IFontStyle
+                  palette: IThemePalette }
+  
+  let createTheme: (obj -> obj) = importMember<obj -> obj> ImportPath
+  
   let loadTheme: (obj -> unit) = importMember<obj -> unit> ImportPath
 
